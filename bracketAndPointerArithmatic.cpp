4a60a8ba2bd51144d6050c13fdda82adb72d8460
avizyt <avijit.biswas27@outlook.com>
2021-05-28 11:19:47 +0530

    New algo file add

---
+++ bracketAndPointerArithmatic.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+
+int main()
+{
+    char lower[] = "abc?e";
+    char upper[] = "ABC?E";
+    char* upper_ptr = &upper[0];
+
+    *(lower + 3) = 'd';
+    *(upper_ptr + 3) = 'D';
+
+    char letter_d = *(lower + 4);
+    char letter_D = *(upper_ptr + 4);
+
+    printf("lower: %s\n upper: %s", lower, upper);
+
+    *(lower + 7) = 'g';
+}
